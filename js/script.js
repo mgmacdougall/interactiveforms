a915da305b1f61c755b5d769e9a26f52b2f3ce8f
Mike MacDougall <MacD>
2020-08-28 17:18:23 -0400

    Add of the 'Job Role' section feature

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 let nameField = document.querySelector('#name');
+let occupationContainer = document.querySelector('.occupation');
+let userTitleSelection = document.querySelector('#title');
+
+const selectionOther = 'other';
 
 window.addEventListener('load', (e) => {
 	nameField.focus();
+	occupationContainer.style.display = 'none';
 });
+
+userTitleSelection.addEventListener('change', (e) => {
+	e.preventDefault();
+	let selection = e.target.value;
+	if (selection === selectionOther) {
+		displayOtherInput();
+	}
+});
+
+function displayOtherInput() {
+	occupationContainer.style.display = 'block';
+}
