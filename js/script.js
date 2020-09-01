410f6f0435af226f1cb098086478911eb3349525
Mike MacDougall <MacD>
2020-09-01 15:26:09 -0400

    Changes to correct the drop down functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let nameField = document.querySelector('#name');
 let occupationContainer = document.querySelector('.occupation');
 let userTitleSelection = document.querySelector('#title');
-let tShirtDesignSelection = document.querySelector('#design');
+let tShirtDesignSelection = document.getElementById('design');
 let tShirtColorSelect = document.querySelector('#color');
 let tShirtColorSelectionOptions = document.getElementById('color');
 
@@ -9,6 +9,7 @@ let tShirtColorSelectionOptionsArray = Array.from(tShirtColorSelectionOptions.ge
 
 const selectionOther = 'other';
 
+// to reset page on load
 window.addEventListener('load', (e) => {
 	nameField.focus();
 	hideOtherInput();
@@ -28,18 +29,25 @@ tShirtDesignSelection.addEventListener('change', (e) => {
 	e.preventDefault();
 	reset(); // reset the values before starting
 	const tshirtData = [
-		{ selection: '', disabledcolors: [] },
+		{ selection: 'Select Theme', disabledcolors: [], defaultColor: '' },
 		{
 			selection: 'js puns',
 			disabledcolors: ['tomato', 'steelblue', 'dimgrey'],
+			defaultColor: 'cornflowerblue',
 		},
 		{
 			selection: 'heart js',
 			disabledcolors: ['cornflowerblue', 'darkslategrey', 'gold'],
+			defaultColor: 'tomato',
 		},
 	];
 	let tShirtSelectionType = e.target.selectedIndex; // gets the selected index of the item selected
 	invokeSelection(tshirtData[tShirtSelectionType].selection, tshirtData); // passes data to the invoke Selection
+
+	// default selection:
+	let defaultSelection = tshirtData[tShirtSelectionType].defaultColor;
+	setDefaultTshirtColorOption(defaultSelection);
+	updateDesignDropDown();
 });
 
 // Selects the drop down item with the data provided
@@ -47,9 +55,8 @@ function invokeSelection(option, data) {
 	for (let item in data) {
 		if (data[item].selection === option) {
 			for (let color in tShirtColorSelectionOptionsArray) {
-				console.log(tShirtColorSelectionOptionsArray[color].value);
 				if (data[item].disabledcolors.includes(tShirtColorSelectionOptionsArray[color].value)) {
-					tShirtColorSelectionOptionsArray[color].disabled = true;
+					tShirtColorSelectionOptionsArray[color].hidden = true;
 				}
 			}
 		}
@@ -59,11 +66,23 @@ function invokeSelection(option, data) {
 // Reset function to reset all the values in the drop downs
 function reset() {
 	tShirtColorSelectionOptionsArray.forEach((v) => {
-		console.log(v.disabled);
 		if (v.value) {
-			v.disabled = false;
+			v.hidden = false;
 		}
+		updateDesignDropDown();
 	});
+}
+
+// sets the default value displaying in the drop down selection
+function setDefaultTshirtColorOption(color) {
+	document.querySelector(`[value='${color}']`).selected = 'true'; // sets the default value
+}
+
+// Resets the TShirt design drop down
+function updateDesignDropDown() {
+	let designThemeDefault = document.querySelector('#design').firstElementChild;
+	let isHidden = designThemeDefault.hidden;
+	isHidden ? (designThemeDefault.hidden = 'false') : (designThemeDefault.hidden = 'true');
 }
 
 function displayOtherInput() {
